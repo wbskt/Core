cf3c6abb68476098235b8aa13efb6b223fd177ff
Richard Joy <richardjoy530@gmail.com>
2025-05-01 11:26:35 +0530

    (chore): folder structure cleanup

--- Wbskt.Common/Contracts/SocketServerHealth.cs
+++ Wbskt.Common/Contracts/SocketServerHealth.cs
@@ -2,14 +2,14 @@ namespace Wbskt.Common.Contracts;
 
 public class SocketServerHealth
 {
-    public Connection[] ActiveConnections { get; set; }
+    public Connection[] ActiveConnections { get; set; } = [];
 }
 
 public class Connection
 {
-    public string ClientName { get; set; }
+    public string ClientName { get; set; } = string.Empty;
 
-    public string ChannelName { get; set; }
+    public string ChannelName { get; set; } = string.Empty;
 }
 
 
