1a547c904408594f89c22a2d547cea5ff130f5e7
Richard Joy <richardjoy530@gmail.com>
2025-05-06 19:11:03 +0530

    (chore): add assembly attributes for internal visibility to unit tests

---
+++ Wbskt.Common/Assembly.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Wbskt.Bdd.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010049154548778aa0" +
+                              "c268c74b862324ec635ca6e2a95189aee7e97543a895ddf1f97eec5f0f3ab219aa609437e51306" +
+                              "9d3acff867fae15e538c071a7f78ab614fe25ae45a8f380821f0012468a5006b5be781d076f31f" +
+                              "6fe7ef614426d56a53610a6489a49d0d68868d28fc6054cbcf5146b8611111a171a274ae884065" +
+                              "739a3dda")]
