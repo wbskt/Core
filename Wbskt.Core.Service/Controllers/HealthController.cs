94b64aafadf36a225d8e4e92978205b59b41992d
Richard Joy <richardjoy530@gmail.com>
2025-05-11 19:38:18 +0530

    (feat): enhance HealthController to manage WebSocket connections and server status

--- Wbskt.Core.Service/Controllers/HealthController.cs
+++ Wbskt.Core.Service/Controllers/HealthController.cs
@@ -1,13 +1,16 @@
 using System.Net.WebSockets;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Wbskt.Common;
 using Wbskt.Common.Contracts;
 using Wbskt.Common.Extensions;
+using Wbskt.Core.Service.Services;
 
 namespace Wbskt.Core.Service.Controllers;
 
 [Route("")]
 [ApiController]
-public class HealthController(ILogger<HealthController> logger) : ControllerBase
+public class HealthController(ILogger<HealthController> logger, IServerInfoService serverInfoService) : ControllerBase
 {
     [HttpGet]
     public IActionResult Ping()
@@ -17,19 +20,25 @@ public class HealthController(ILogger<HealthController> logger) : ControllerBase
     }
 
     [HttpGet("/ws")]
+    [Authorize(AuthenticationSchemes = Constants.AuthSchemes.SocketServerScheme)]
     public async Task Get()
     {
+        var sid = User.GetSocketServerId();
         if (HttpContext.WebSockets.IsWebSocketRequest)
         {
             try
             {
                 using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-                await Echo(webSocket);
+                serverInfoService.UpdateServerStatus(sid, true);
+                await Echo(webSocket, Program.Cts.Token);
             }
             catch (Exception ex)
             {
                 logger.LogError("unexpected error occured while maintaining connection to socket server: {error}", ex.Message);
-                HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+            finally
+            {
+                serverInfoService.UpdateServerStatus(sid, false);
             }
         }
         else
@@ -38,15 +47,29 @@ public class HealthController(ILogger<HealthController> logger) : ControllerBase
         }
     }
 
-    private static async Task Echo(WebSocket webSocket)
+    private async Task Echo(WebSocket ws, CancellationToken ct)
     {
-        var result = await webSocket.ReadAsync();
+        ct.Register(() => CloseClientConnection(logger, ws).Wait(CancellationToken.None));
+        var result = await ws.ReadAsync(CancellationToken.None);
 
         while (!result.ReceiveResult.CloseStatus.HasValue)
         {
-            result = await webSocket.ReadAsync();
+            result = await ws.ReadAsync(CancellationToken.None);
         }
 
-        await webSocket.CloseAsync(result.ReceiveResult.CloseStatus.Value, result.ReceiveResult.CloseStatusDescription, CancellationToken.None);
+        if (ws.State is WebSocketState.Open or WebSocketState.CloseReceived or WebSocketState.CloseSent)
+        {
+            logger.LogInformation("closing connection ({closeStatus})", result.ReceiveResult.CloseStatusDescription);
+            await ws.CloseAsync(result.ReceiveResult.CloseStatus.Value, result.ReceiveResult.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+
+    private static async Task CloseClientConnection(ILogger logger, WebSocket ws)
+    {
+        if (ws.State is WebSocketState.Open or WebSocketState.CloseReceived or WebSocketState.CloseSent)
+        {
+            logger.LogInformation("closing connection ({closeStatus})", "Closing connection (client initiated)");
+            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing connection (client initiated)", CancellationToken.None);
+        }
     }
 }
